4485c2a92acadbeeddbb3e6d2f0852260f678b72
Fred <fred@fastmail.nl>
2022-02-18 09:14:56 +0100

    Add form response element

---
+++ src/components/FormResponse.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="fixed bottom-0 right-0 w-72 h-28 p-6 bg-sky-200 shadow-md">
+    <p>Form resonse</p>
+    <button class="absolute top-0 right-0 bg-gray-700 text-white py-2 px-4">
+      X
+    </button>
+  </div>
+</template>
+
+<style></style>
