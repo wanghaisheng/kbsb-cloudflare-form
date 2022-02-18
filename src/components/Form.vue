b5008da9b83935731f44c8de71cb3e8388fed54a
Fred <fred@fastmail.nl>
2022-02-18 14:35:42 +0100

    Add hcaptcha script and element. Add Cloudflare worker URL to form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,11 +3,32 @@ import FormInput from './FormInput.vue';
 import FormTextarea from './FormTextarea.vue';
 import FormResponse from './FormResponse.vue';
 import Button from './Button.vue';
+
+function submitForm() {
+  console.log('Submitted');
+  // Todo: refactor submit, remove form action
+  // https://kbsb-cloudflare-form-worker.kaboom.workers.dev
+  // @submit.prevent="submitForm"
+}
+</script>
+
+<script>
+export default {
+  mounted() {
+    const captcha = document.createElement('script');
+    captcha.setAttribute('src', 'https://js.hcaptcha.com/1/api.js');
+    document.head.appendChild(captcha);
+  },
+};
 </script>
 
 <template>
   <section class="py-10">
-    <form class="flex flex-col items-center bg-gray-100 py-4" action="">
+    <form
+      class="flex flex-col items-center bg-gray-100 py-4"
+      action="https://kbsb-cloudflare-form-worker.kaboom.workers.dev"
+      method="POST"
+    >
       <FormInput
         id="first_name"
         label="First name"
@@ -55,6 +76,17 @@ import Button from './Button.vue';
         label="Yes, I would like to stay updated"
       />
       <Button text="Send" />
+      <div
+        class="h-captcha"
+        data-sitekey="d34868ed-69b1-4794-9516-5e3f0f802bd0"
+      ></div>
+      <Button type="submit" text="Send" />
+      <div>
+        This site is protected by
+        <a href="https://www.hCaptcha.com">hCaptcha</a> and its
+        <a href="https://www.hcaptcha.com/privacy">Privacy Policy</a> and
+        <a href="https://www.hcaptcha.com/terms">Terms of Service</a> apply.
+      </div>
     </form>
     <FormResponse />
   </section>
