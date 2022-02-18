6c88e30d4aa30bf94b72845171a8683f17667354
Fred <fred@fastmail.nl>
2022-02-18 01:51:59 +0100

    Add FormTextArea

---
+++ src/components/FormTextarea.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="">
+    <label class="block mr-4" :for="id">{{ label }}</label>
+    <textarea
+      class="w-80 inline-block bg-gray-300 my-2 p-2"
+      :id="id"
+      :name="id"
+      :minlength="minLength"
+      :maxlength="maxLength"
+      :required="isRequired"
+      :placeholder="placeholder"
+      :pattern="pattern"
+      :rows="rows"
+      :cols="cols"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    label: String,
+    placeholder: String,
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    minLength: {
+      type: String,
+      default: '2',
+    },
+    maxLength: {
+      type: String,
+      default: '',
+    },
+    pattern: String,
+  },
+};
+</script>
