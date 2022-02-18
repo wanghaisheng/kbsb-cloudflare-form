d1bec57a6c43f4efd4a50793c375c1c3a3a3409f
Fred <fred@fastmail.nl>
2022-02-18 01:53:01 +0100

    Add props

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="">
-    <label class="mr-4" for="first_name">First name</label>
     <input
-      class="inline-block bg-gray-300 my-2 p-2"
-      type="text"
-      id="first_name"
-      name="first_name"
-      minlength="2"
-      required
-      pattern="^[a-zA-Z- .,&]*$"
-      placeholder="First name"
+      :type="type"
+      :id="id"
+      :name="id"
+      :minlength="minLength"
+      :maxlength="maxLength"
+      :required="isRequired"
+      :placeholder="placeholder"
+      :pattern="pattern"
     />
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  props: {
+    id: String,
+    type: {
+      type: String,
+      default: 'text',
+      validator(value) {
+        // The value must match one of these strings
+        return ['text', 'tel', 'email', 'checkbox'].includes(value);
+      },
+    },
+    label: String,
+    placeholder: String,
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    minLength: {
+      type: String,
+      default: '2',
+    },
+    maxLength: {
+      type: String,
+      default: '',
+    },
+    pattern: String,
+  },
+};
+</script>
