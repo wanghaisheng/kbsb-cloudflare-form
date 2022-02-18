ef104862b9e8a2a061e3f3154e9d745a96cce577
Fred <fred@fastmail.nl>
2022-02-18 14:36:01 +0100

    Fix typo

--- src/components/FormResponse.vue
+++ src/components/FormResponse.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="fixed bottom-0 right-0 w-72 h-28 p-6 bg-sky-200 shadow-md">
-    <p>Form resonse</p>
+    <p>Form response</p>
     <button class="absolute top-0 right-0 bg-gray-700 text-white py-2 px-4">
       X
     </button>
